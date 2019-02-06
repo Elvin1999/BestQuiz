c24df1d307aca0f8d19091ac744526e05cf09ff8
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-06 18:10:37 +0400

    Okay

--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -9,7 +9,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
-
 namespace Quiz
 {
     public partial class Form1 : Form
@@ -23,57 +22,76 @@ namespace Quiz
         {
             Application.Exit();
         }
-        public List<QuestionBlock> QuestionBlock { get; set; }
-        public List<QuestionControl> questionControls { get; set; }
+        public List<QuestionBlock> QuestionList { get; set; }
         public int CurrentIndex { get; set; }
         RadioButton radioButton = new RadioButton();
-        private int y = 0; 
         private void Form1_Load(object sender, EventArgs e)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(QuestionBlock[]));
-            questionControls = new List<QuestionControl>();
             if (File.Exists("QuestionsXML.xml"))
             {
-
                 using (FileStream f = new FileStream("QuestionsXML.xml", FileMode.Open))
                 {
-                    QuestionBlock = (serializer.Deserialize(f) as QuestionBlock[]).ToList();
+                    QuestionList = (serializer.Deserialize(f) as QuestionBlock[]).ToList();
                 }
+                labelQuestion.Text = QuestionList[CurrentIndex].Text;
+                int y = 0;
+                for (int k = 0; k < QuestionList[CurrentIndex].Answers.Count; k++)
+                {
+                    RadioButton radioButton = new RadioButton();
+                    radioButton.Size = new Size(350, 60);
+                    radioButton.Location = new Point(56, 192+y);
+                    radioButton.Text= QuestionList[CurrentIndex].Answers[k].Text;
+                    radioButton.Font = new Font("Century", 10, FontStyle.Italic);
+                    y += 60;
+                    this.Controls.Add(radioButton);
+                }
+                int i = 0;
 
-                labelQuestion.Text = QuestionBlock[CurrentIndex].Text;
-                QuestionControl controls = new QuestionControl(QuestionBlock[CurrentIndex]);
-                questionControls.Add(controls);
-                this.Controls.AddRange(questionControls.ToArray());
             }
         }
         private void metroBackbtn_Click(object sender, EventArgs e)
         {
-            for (int k = 0; k < questionControls[CurrentIndex].ListRadiobutton.Count; k++)
+            --CurrentIndex;int y = 0;
+            foreach (var item in this.Controls)
             {
-                questionControls[CurrentIndex].ListRadiobutton[k].Visible = false;
+                if (item is RadioButton rb)
+                {
+                    rb.Dispose();
+                }
             }
-            --CurrentIndex;
-            labelQuestion.Text = QuestionBlock[CurrentIndex].Text;
-            QuestionControl controls = new QuestionControl(QuestionBlock[CurrentIndex]);
-            questionControls.Add(controls);
-            this.Controls.AddRange(questionControls.ToArray());
+            for (int k = 0; k < QuestionList[CurrentIndex].Answers.Count; k++)
+            {
+                RadioButton radioButton = new RadioButton();
+                radioButton.Size = new Size(350, 60);
+                radioButton.Location = new Point(56, 192 + y);
+                radioButton.Text = QuestionList[CurrentIndex].Answers[k].Text;
+                radioButton.Font = new Font("Century", 10, FontStyle.Italic);
+                y += 60;
+                this.Controls.Add(radioButton);
+            }
         }
-
         private void metroNextbtn_Click(object sender, EventArgs e)
         {
-
-            for (int k = 0; k < questionControls[CurrentIndex].ListRadiobutton.Count; k++)
+            ++CurrentIndex;int y = 0;
+            foreach (var item in this.Controls)
             {
-                questionControls[CurrentIndex].ListRadiobutton[k].Visible = false;
+                if(item is RadioButton rb)
+                {
+                    rb.Dispose();
+                }
             }
-
-            ++CurrentIndex;
-            labelQuestion.Text = QuestionBlock[CurrentIndex].Text;
-            QuestionControl controls = new QuestionControl(QuestionBlock[CurrentIndex]);
-            questionControls.Add(controls);
-            this.Controls.AddRange(questionControls.ToArray());
+            for (int k = 0; k < QuestionList[CurrentIndex].Answers.Count; k++)
+            {
+                RadioButton radioButton = new RadioButton();
+                radioButton.Size = new Size(350, 60);
+                radioButton.Location = new Point(56, 192 + y);
+                radioButton.Text = QuestionList[CurrentIndex].Answers[k].Text;
+                radioButton.Font = new Font("Century", 10, FontStyle.Italic);
+                y += 60;
+                this.Controls.Add(radioButton);
+            }
         }
-
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
