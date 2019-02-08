b22a570214790d9cff5a65d77239d3b94334cabf
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-08 15:41:03 +0400

    Some Design was added . . .

---
+++ Quiz/ProgramIntro.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Quiz
+{
+    partial class ProgramIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.labelDateTime = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.panelMiddle = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panelHeader.SuspendLayout();
+            this.panelMiddle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelHeader.Controls.Add(this.labelDateTime);
+            this.panelHeader.Controls.Add(this.labelX);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(800, 71);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // labelDateTime
+            // 
+            this.labelDateTime.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDateTime.ForeColor = System.Drawing.Color.White;
+            this.labelDateTime.Location = new System.Drawing.Point(3, 3);
+            this.labelDateTime.Name = "labelDateTime";
+            this.labelDateTime.Size = new System.Drawing.Size(189, 24);
+            this.labelDateTime.TabIndex = 0;
+            this.labelDateTime.Text = "X";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX.ForeColor = System.Drawing.Color.White;
+            this.labelX.Location = new System.Drawing.Point(770, 1);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(27, 24);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "X";
+            this.labelX.Click += new System.EventHandler(this.labelX_Click);
+            // 
+            // panelMiddle
+            // 
+            this.panelMiddle.BackColor = System.Drawing.Color.DarkGray;
+            this.panelMiddle.Controls.Add(this.button2);
+            this.panelMiddle.Controls.Add(this.button1);
+            this.panelMiddle.Controls.Add(this.labelTitle);
+            this.panelMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMiddle.Location = new System.Drawing.Point(0, 71);
+            this.panelMiddle.Name = "panelMiddle";
+            this.panelMiddle.Size = new System.Drawing.Size(800, 379);
+            this.panelMiddle.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(3, 23);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(325, 125);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "EasyQuiz";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(140, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create New Test";
+            this.button1.UseVisualStyleBackColor = false;
+  
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(410, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Take an examination";
+            this.button2.UseVisualStyleBackColor = false;
+      
+            // 
+            // ProgramIntro
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelMiddle);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProgramIntro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProgramIntro";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelMiddle.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelMiddle;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelDateTime;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Quiz/ProgramIntro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class ProgramIntro : Form
+    {
+        public ProgramIntro()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;timer.Start();
+            labelDateTime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            labelDateTime.Text = DateTime.Now.AddSeconds(1).ToLongTimeString();
+        }
+
+        private void labelX_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+      
+    }
+}
